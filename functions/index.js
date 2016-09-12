c6c2f1f55b2946a2b3d589a10aaaed16b412cc01
prescottprue <sprue.dev@gmail.com>
2016-09-12 02:54:47 -0700

    createZip function successfully deployed.

--- functions/index.js
+++ functions/index.js
@@ -1,58 +1,40 @@
 var functions = require('firebase-functions');
 // var AdmZip = require('adm-zip');
 // var gcloud = require('gcloud');
-// var firebase = require('firebase');
+var firebase = require('firebase');
 // var _ = require('lodash');
 // var Promise = require('es6-promise').Promise
 // global.Promise = Promise
-//
-// var jsdom = require('jsdom')
-// var doc = jsdom.jsdom('<html><body></body></html>')
-// var winow = doc.defaultView
-// global.document = doc
-// global.window = window
-// global.navigator = window.navigator
 
-// // converts the "text" key of messages pushed to /messages to uppercase
-exports.upperCaser = functions.database().path('/messages/{id}').on('write', function(event) {
-  // prevent infinite loops
-  if (event.data.child('uppercased').val()) { return; }
+exports.createZip = functions.database().path('/requests/{id}').on('write', function(e) {
+  if (e.data.child('isCompleted').val()) { return }
+  // var Firepad = require('firepad');
+  try {
+    firebase.initializeApp({
+      serviceAccount: './serviceAccount.json',
+      databaseURL: "https://pruvit-968.firebaseio.com"
+    });
+  } catch(err) {}
 
-  return event.data.ref.update({
-    text: event.data.child('text').val().toUpperCase(),
-    uppercased: true
-  });
-});
 
-// exports.createZip = functions.database().path('/requests/{id}').on('write', function(e) {
-//   if (e.data.child('isCompleted').val()) { return }
-//   // var Firepad = require('firepad');
-//
-//   firebase.initalizeApp({
-//     apiKey: "AIzaSyBuwR21cO0lMzMr_T-Dl_jG1dsORXZ1fwY",
-//      authDomain: "devshare-stg.firebaseapp.com",
-//      databaseURL: "https://devshare-stg.firebaseio.com",
-//      storageBucket: "devshare-stg.appspot.com"
-//   });
-//
-//   return firebase.database()
-//     .ref('files/prescottprue/diuy/doit:js')
-//     .once('value')
-//     .then(fileSnap =>  e.data.ref.update({
-//         isCompleted: true,
-//         val: fileSnap.val()
-//       })
-//       // Firepad.Headless(fileSnap.ref).getText(text => {
-//       //   // zip.addFile(child.meta.path, new Buffer(text))
-//       //   // resolve(text || '')
-//       //   console.log('text from firepad:', text);
-//       //   return e.data.ref.update({
-//       //     isCompleted: true,
-//       //     text: text
-//       //   })
-//       // })
-//     )
-// })
+  return firebase.database()
+    .ref('files/prescottprue/diuy/doit:js')
+    .once('value')
+    .then(fileSnap =>  e.data.ref.update({
+        isCompleted: true,
+        val: fileSnap.val()
+      })
+      // Firepad.Headless(fileSnap.ref).getText(text => {
+      //   // zip.addFile(child.meta.path, new Buffer(text))
+      //   // resolve(text || '')
+      //   console.log('text from firepad:', text);
+      //   return e.data.ref.update({
+      //     isCompleted: true,
+      //     text: text
+      //   })
+      // })
+    )
+})
 
 // function createZip (directory) {
 //   let zip = new AdmZip()
