7cf0ad38325dc5ba2f28d8e5b48c1d563e1a2d8f
prescottprue <sprue.dev@gmail.com>
2017-02-22 00:51:30 -0800

    Removed Firebase info from config (it comes from devshare library).

--- build/app-config.js
+++ build/app-config.js
@@ -2,35 +2,25 @@
 const _debug = require('debug') // eslint-disable-line no-underscore-dangle
 const fs = require('fs')
 const path = require('path')
-const firebaseEnvironemnts = require('../config/firebaseEnvironments')
 
 const debug = _debug('app:build:config')
 const {
   TRAVIS_BRANCH,
   TRAVIS_PULL_REQUEST,
   SENTRY_DSN,
-  GA_TRACKINGID,
-  INT_FIREBASE_WEBAPIKEY,
-  STAGE_FIREBASE_WEBAPIKEY,
-  PROD_FIREBASE_WEBAPIKEY
+  GA_TRACKINGID
 } = process.env
 
 const createConfigFile = (cb) => {
   let env = 'int'
-  let firebase = firebaseEnvironemnts.int
-  firebase.apiKey = INT_FIREBASE_WEBAPIKEY
 
   if (TRAVIS_PULL_REQUEST === 'false') {
     switch (TRAVIS_BRANCH) {
       case 'stage':
         env = 'stage'
-        firebase = firebaseEnvironemnts.stage
-        firebase.apiKey = STAGE_FIREBASE_WEBAPIKEY
         break
       case 'prod':
         env = 'prod'
-        firebase = firebaseEnvironemnts.prod
-        firebase.apiKey = PROD_FIREBASE_WEBAPIKEY
         break
       default:
         // leave int by default
@@ -38,7 +28,7 @@ const createConfigFile = (cb) => {
   }
 
   const outputPath = path.join(__dirname, '..', 'src/config.js')
-  const fileString = `export const firebase = ${JSON.stringify(firebase, null, 2)}\n` +
+  const fileString =
     `\nexport const env = ${JSON.stringify(env)}\n` +
     `\nexport const sentryDsn = ${JSON.stringify(SENTRY_DSN || '')}\n` +
     `\nexport const gaTrackingId = ${JSON.stringify(GA_TRACKINGID || '')}\n` +
