d62f002d080d9235e2956182e72054f1e389579e
Sinan Bolel <sinanbolel@gmail.com>
2017-03-10 02:11:39 -0600

    fix(specs): Login, Signup routes

--- tests/routes/Login/index.spec.js
+++ tests/routes/Login/index.spec.js
@@ -13,12 +13,14 @@ describe('(Route) Login', () => {
     expect(LoginRoute).to.be.a.function
   })
 
-  it('Sets Path to :username', () => {
-    expect(_route.path).to.equal('login')
+  it('Sets Path to /login', () => {
+    expect(_route.path).to.equal('/login')
   })
+
   it('Defines a getComponent function', () => {
     expect(_route.getComponent).to.be.a.function
   })
+
   it('Defines a getChildRoutes function', () => {
     expect(_route.getChildRoutes).to.be.a.function
   })
--- tests/routes/Signup/index.spec.js
+++ tests/routes/Signup/index.spec.js
@@ -13,12 +13,14 @@ describe('(Route) Signup', () => {
     expect(SignupRoute).to.be.a.function
   })
 
-  it('Sets Path to :username', () => {
-    expect(_route.path).to.equal('signup')
+  it('Sets Path to /signup', () => {
+    expect(_route.path).to.equal('/signup')
   })
+
   it('Defines a getComponent function', () => {
     expect(_route.getComponent).to.be.a.function
   })
+
   it('Defines a getChildRoutes function', () => {
     expect(_route.getChildRoutes).to.be.a.function
   })
